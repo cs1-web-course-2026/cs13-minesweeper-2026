573fe5b132f2626bdfd0c8290b59eab0dc8bd29a
YevhenSkala <skala.yevhen@student.karazin.ua>
2026-04-07 18:41:06 +0200

    Функція генерації ігрової сітки та розстановки мін

--- SkalaYevhen/script.js
+++ SkalaYevhen/script.js
@@ -7,3 +7,38 @@ const gameState = {
     timerId: null,
     field: [] 
 };
+function initGame() {
+    clearInterval(gameState.timerId);
+    gameState.status = 'process';
+    gameState.gameTime = 0;
+    gameState.timerId = null;
+    gameState.field = generateField(gameState.rows, gameState.cols, gameState.minesCount);
+    calculateAllNeighbors();
+    startTimer();
+    console.log("Гра розпочата", gameState.field);
+}
+
+function generateField(rows, cols, minesCount) {
+    const field = [];
+    for (let r = 0; r < rows; r++) {
+        const row = [];
+        for (let c = 0; c < cols; c++) {
+            row.push({
+                type: 'empty',
+                neighborMines: 0,
+                state: 'closed'
+            });
+        }
+        field.push(row);
+    }
+    let minesPlaced = 0;
+    while (minesPlaced < minesCount) {
+        const r = Math.floor(Math.random() * rows);
+        const c = Math.floor(Math.random() * cols);
+        if (field[r][c].type !== 'mine') {
+            field[r][c].type = 'mine';
+            minesPlaced++;
+        }
+    }
+    return field;
+}
